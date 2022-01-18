0c731fe1917eb2f5b75944dff67daaf37d3883f4
Srilekha <janagamsrilekha1997@gmail.com>
2022-01-18 10:45:47 -0600

    Added the demo file

---
+++ 01Demo.playground/Contents.swift
@@ -0,0 +1,60 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+print("Hi", 0.2)
+print(greeting)
+
+var  person1="Anshuman"
+print("I Hate \(person)")
+
+var name="Ajay"
+
+print("Hello, \(name)!")
+
+var num=3
+var num1=3
+print("Sum of two numbers:\(num+2)")
+
+print("""
+Hello
+""")
+
+let message:String="hello"
+
+print(message)
+
+print("welcome to swift")
+print("Fall 2021")
+
+
+
+let grade:String="A"
+
+print("hello, I am Srilekha.",terminator:" I have secured \(grade) grade.")
+
+
+print("The list of items")
+print(1,2,3)
+print("The new pattern")
+print(1,2,3 , separator:"-")
+
+
+let pi:Int=3
+let r:Int=2
+var area=2*pi*r
+print(area)
+
+var Name="Srilekha"
+print(Name)
+print("Name")
+
+var  httpError=(errorCode :404 , errorMesage : "Page Not Found")
+print(httpError)
+print(httpError.errorCode)
+
+var person=(name:"Srilekha ", age1:"24years" , height:"5.4cms",name1:(a:"Janagam",b:"4"))
+            print(person.name)
+print(person.name1)
+
+
